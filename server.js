7545e4ff905a7115d3dcad8f688874a9440c6cba
joaquin solis <sol19008@byui.edu>
2024-03-23 23:52:12 -0300

    add week3

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const static = require("./routes/static");
 const expressLayouts = require("express-ejs-layouts");
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities/");
+
+// week 4
+/* const session = require("express-session")
+const pool = require('./database/') */
 
 /* ***********************
  * View Engine and Template
@@ -20,6 +25,18 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout"); // not at views root
 
+// week 4
+/* app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+ */
 /* ***********************
  * Routes
  *************************/
@@ -29,6 +46,26 @@ app.use("/inv", inventoryRoute);
 
 // index route
 app.get("/", baseController.buildHome);
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message: err.message,
+    nav,
+  });
+});
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
